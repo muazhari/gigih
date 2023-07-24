4fdddd8e648c232bd23f9483b2ea8560cce94f3c
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2023-07-24 19:39:09 +0700

    Update m4-s1.

---
+++ module-4/session-1/homework-1/src/components/Content.tsx
@@ -0,0 +1,139 @@
+import "./Content.css"
+
+export default function Content() {
+    return (
+        <>
+            <div className="content">
+                <div className="section" id="all-song">
+                    <div className="title">
+                        <h1>All Song</h1>
+                    </div>
+                    <div className="song-list">
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="section" id="most-played-song">
+                    <div className="title">
+                        <h1>Most Played Songs</h1>
+                    </div>
+                    <div className="song-list">
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="section" id="playlisted-song">
+                    <div className="title">
+                        <h1>Playlisted Songs</h1>
+                    </div>
+                    <div className="song-list">
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                        <div className="song">
+                            <div className="song-image">
+                                <img src="https://placehold.co/200" alt="song-image"/>
+                            </div>
+                            <div className="song-info">
+                                <h3>Song name</h3>
+                                <p>Customer name</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ module-4/session-1/homework-1/src/components/SideBar.tsx
@@ -0,0 +1,26 @@
+import SpotifyLogo from "../assets/spotify-logo-with-text.svg";
+import "./SideBar.css";
+
+export default function SideBar() {
+    return (
+        <>
+            <div className="side-bar">
+                <div className="logo">
+                    <img src={SpotifyLogo} alt="logo"/>
+                </div>
+                <div className="menu">
+                    <a className="menu-item" href="#all-songs">
+                        <p>All Songs</p>
+                    </a>
+                    <a className="menu-item" href="#most-played-songs">
+                        <p>Most Played Songs</p>
+                    </a>
+                    <a className="menu-item" href="#playlisted-songs">
+                        <p>Playlisted Songs</p>
+                    </a>
+                </div>
+            </div>
+            <div className="side-bar-behind"></div>
+        </>
+    )
+}
